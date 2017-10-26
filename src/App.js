e7254df167335c72884d92761bcb801ba1282529
Alex Williams <alexwilliams567@gmail.com>
2017-10-26 22:22:37 +0530

    css updates, added size and pause/resume buttons

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ class App extends Component {
    }
   
   render() {
+    const titleStyle = {
+        fontSize: '48px',
+        textAlign: 'center'
+    };
     return(
       <div>
+        <div className="title" style={titleStyle}>Conway's Game of Life</div>
         <Game />
       </div>
     );
@@ -28,10 +33,10 @@ class ControlButtons extends Component {
   render() {
     return (
         <div className="control-buttons">
+          <button className="heightUpdate" onClick={() => this.changeSpecs(70, 50)}>70x50</button>
           <button className="heightUpdate" onClick={() => this.changeSpecs(50, 30)}>50x30</button>
-          <button className="heightUpdate" onClick={() => this.changeSpecs(50, 40)}>50x40</button>
           <button className="heightUpdate" onClick={() => this.changeSpecs(50, 50)}>50x50</button>
-          <button className="heightUpdate" onClick={() => this.props.pause()}>Pause</button>
+          <button className="heightUpdate" onClick={() => this.props.pause()}>{this.props.pauseToggle ? "Resume" : "Pause"}</button>
         </div>
     );
   }
@@ -43,10 +48,12 @@ class Game extends Component {
     this.state = {
       gridWidth: 70,
       gridHeight: 50,
+      gridSize: 3500,
       grid: [],
-      futureGrid: [],
       generationCount: 0,
+      iterationSpeed: 250,
       timer: null,
+      pauseToggle: false
     };
   }
   
@@ -54,36 +61,52 @@ class Game extends Component {
     this.createNewBoard();
   };
 
-  componentWillUnmount() {
-    this.clearInterval(this.state.timer);
-  }
-
   updateGameGridSize = (width, height) => {
+    // $('.game-cells').empty();
+    clearInterval(this.state.timer);
     this.setState({
+      grid: [],
       gridWidth: width,
-      gridHeight: height
-    })
-    this.createNewBoard();
+      gridHeight: height,
+      gridSize: width * height,
+      timer: null,
+      pauseToggle: false
+    });
+    setTimeout(() => { 
+      this.createNewBoard();
+    }, 1000);    
+  
   };
 
-  pause = () => {
-    clearInterval(this.state.timer);
+  togglePause = () => {
+    if (this.state.pauseToggle) {
+      let timer = setInterval(() => {this.nextGeneration()}, this.state.iterationSpeed);
+
+      this.setState({
+        timer: timer
+      });
+    } else {
+      clearInterval(this.state.timer);
+      this.setState({
+        pauseToggle: !this.state.pauseToggle
+      });
+    }
   };
   
   //createBoard
   createNewBoard = () => {
-    let grid = [...this.state.grid];
-    let gridSize = this.state.gridWidth * this.state.gridHeight;
-    for (var i = 0; i < gridSize; i++) {
+    let grid = [];
+    for (var i = 0; i < this.state.gridSize; i++) {
       let element = Math.floor(Math.random() * 2);
       grid.push(element);
     }
     
-    this.setState({ 
+    this.setState({
         grid: grid,
+        generationCount: 0
     });
 
-    let timer = setInterval(() => {this.nextGeneration()}, 500);
+    let timer = setInterval(() => {this.nextGeneration()}, this.state.iterationSpeed);
 
     this.setState({
       timer: timer
@@ -93,13 +116,13 @@ class Game extends Component {
   //run next generation
   nextGeneration = () => {
     let futureGrid = [...this.state.grid];
-    let gridSize = this.state.gridWidth * this.state.gridHeight;
-    for (var i = 0; i < gridSize; i++) {
+    for (var i = 0; i < this.state.gridSize; i++) {
       futureGrid[i] = this.compareCell(i);
-    } 
+    }
 
     this.setState({
-      grid: futureGrid
+      grid: futureGrid,
+      generationCount: this.state.generationCount + 1
     });
   };
   
@@ -110,66 +133,70 @@ class Game extends Component {
     let currentGrid = this.state.grid;
     const row = Number(this.state.gridWidth);
     const height = Number(this.state.gridHeight);
-    //check whether cell is top left corner
-    if (index === 0) {
-      if (currentGrid[1] === 0) aliveNeighborCount++;
-      if (currentGrid[row] === 0) aliveNeighborCount++;
-      if (currentGrid[row + 1] === 0) aliveNeighborCount++;
-     //top right corner 
-    } else if (index === row - 1) {
-      if (currentGrid[row - 2] === 0) aliveNeighborCount++;
-      if (currentGrid[2 * row - 2] === 0) aliveNeighborCount++;
-      if (currentGrid[2 * row - 1] === 0) aliveNeighborCount++;
-      
-    //bottom left corner  
-    } else if (index === (row * height) - row) {
-      if (currentGrid[row * height - 2*row] === 0) aliveNeighborCount++;
-      if (currentGrid[row * height - 2 * row + 1] === 0) aliveNeighborCount++;
-      if (currentGrid[row * height - row + 1] === 0) aliveNeighborCount++;
-    //bottom right corner  
-    } else if (index === (row * height) - 1) {
-      if (currentGrid[row * height - row - 2] === 0) aliveNeighborCount++;
-      if (currentGrid[row * height - row - 1] === 0) aliveNeighborCount++;
-      if (currentGrid[row * height - 2] === 0) aliveNeighborCount++;
-    //check whether cell is first row
-    } else if (index < row) {
-      if (currentGrid[index - 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index + 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index + row - 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index + row] === 0) aliveNeighborCount++;
-      if (currentGrid[index + row + 1] === 0) aliveNeighborCount++;
-      //bottom row
-    } else if (index > (row * height) - row - 1) {
-      if (currentGrid[index - 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index + 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row - 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row + 1] === 0) aliveNeighborCount++;
-      //check left column
-    } else if (index % (row) === 0) {
-      if (currentGrid[index + row] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row] === 0) aliveNeighborCount++;
-      if (currentGrid[index + row + 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index + 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row + 1] === 0) aliveNeighborCount++;
-      //check right column
-    } else if (index % row === (row - 1)) {
-      if (currentGrid[index + row] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row] === 0) aliveNeighborCount++;
-      if (currentGrid[index + row - 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index - 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row - 1] === 0) aliveNeighborCount++;
-      //all neighbor count
-    } else {
-      if (currentGrid[index + row] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row] === 0) aliveNeighborCount++;
-      if (currentGrid[index + 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index - 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index + row + 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row + 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index + row - 1] === 0) aliveNeighborCount++;
-      if (currentGrid[index - row - 1] === 0) aliveNeighborCount++;
-    }
+    if (index < row || index > (row * height) - row - 1) {
+      //check whether cell is top left corner
+        if (index === 0) {
+          if (currentGrid[1] === 0 || currentGrid[1] === 2) aliveNeighborCount++;
+          if (currentGrid[row] === 0 || currentGrid[row] === 2) aliveNeighborCount++;
+          if (currentGrid[row + 1] === 0 || currentGrid[row + 1] === 2) aliveNeighborCount++;
+         //top right corner 
+        } else if (index === row - 1) {
+          if (currentGrid[row - 2] === 0 || currentGrid[row - 2] === 2) aliveNeighborCount++;
+          if (currentGrid[2 * row - 2] === 0 || currentGrid[2 * row - 2] === 2) aliveNeighborCount++;
+          if (currentGrid[2 * row - 1] === 0 || currentGrid[2 * row - 1] === 2) aliveNeighborCount++;
+          
+        //bottom left corner  
+        } else if (index === (row * height) - row) {
+          if (currentGrid[row * height - 2 * row] === 0 || currentGrid[row * height - 2 * row] === 2) aliveNeighborCount++;
+          if (currentGrid[row * height - 2 * row + 1] === 0 || currentGrid[row * height - 2 * row + 1] === 2) aliveNeighborCount++;
+          if (currentGrid[row * height - row + 1] === 0 || currentGrid[row * height - row + 1] === 2) aliveNeighborCount++;
+        //bottom right corner  
+        } else if (index === (row * height) - 1) {
+          if (currentGrid[row * height - row - 2] === 0 || currentGrid[row * height - row - 2] === 2) aliveNeighborCount++;
+          if (currentGrid[row * height - row - 1] === 0 || currentGrid[row * height - row - 1] === 2) aliveNeighborCount++;
+          if (currentGrid[row * height - 2] === 0 || currentGrid[row * height - 2] === 2) aliveNeighborCount++;
+        //check whether cell is first row
+        } else if (index < row) {
+          if (currentGrid[index - 1] === 0 || currentGrid[index - 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index + 1] === 0 || currentGrid[index + 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index + row - 1] === 0 || currentGrid[index + row - 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index + row] === 0 || currentGrid[index + row] === 2) aliveNeighborCount++;
+          if (currentGrid[index + row + 1] === 0 || currentGrid[index + row + 1] === 2) aliveNeighborCount++;
+          //bottom row
+        } else if (index > (row * height) - row - 1) {
+          if (currentGrid[index - 1] === 0 || currentGrid[index - 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index + 1] === 0 || currentGrid[index + 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row - 1] === 0 || currentGrid[index - row - 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row] === 0 || currentGrid[index - row] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row + 1] === 0 || currentGrid[index - row + 1] === 2) aliveNeighborCount++;
+        } 
+      } else {
+        //check left column
+        if (index % (row) === 0) {
+          if (currentGrid[index + row] === 0 || currentGrid[index + row] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row] === 0 || currentGrid[index - row] === 2) aliveNeighborCount++;
+          if (currentGrid[index + row + 1] === 0 || currentGrid[index + row + 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index + 1] === 0 || currentGrid[index + 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row + 1] === 0 || currentGrid[index - row + 1] === 2) aliveNeighborCount++;
+          //check right column
+        } else if (index % row === (row - 1)) {
+          if (currentGrid[index + row] === 0 || currentGrid[index + row] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row] === 0 || currentGrid[index - row] === 2) aliveNeighborCount++;
+          if (currentGrid[index + row - 1] === 0 || currentGrid[index + row - 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index - 1] === 0 || currentGrid[index - 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row - 1] === 0 || currentGrid[index - row - 1] === 2) aliveNeighborCount++;
+          //all neighbor count
+        } else {
+          if (currentGrid[index + row] === 0 || currentGrid[index + row] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row] === 0 || currentGrid[index - row] === 2) aliveNeighborCount++;
+          if (currentGrid[index + 1] === 0 || currentGrid[index + 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index - 1] === 0 || currentGrid[index - 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index + row + 1] === 0 || currentGrid[index + row + 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row + 1] === 0 || currentGrid[index - row + 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index + row - 1] === 0 || currentGrid[index + row - 1] === 2) aliveNeighborCount++;
+          if (currentGrid[index - row - 1] === 0 || currentGrid[index - row - 1] === 2) aliveNeighborCount++;
+        }
+      }
     
     if (currentCell === 0) {
         if (aliveNeighborCount === 2 || aliveNeighborCount === 3) {
@@ -179,55 +206,72 @@ class Game extends Component {
           // else cell dies due to over/under population
           return 1;
         }
-      } else if (currentCell === 1) {
+    } else if (currentCell === 2) {
+        if (aliveNeighborCount === 2 || aliveNeighborCount === 3) {
+          return 0;
+        } else {
+          // else cell dies due to over/under population
+          return 1;
+        }
+    } else if (currentCell === 1) {
         if (aliveNeighborCount === 3) {
           // if 3 alive neighbors, dead cell becomes alive
-          return 0;
+          return 2;
         } else {
           // if not exactly 3 alive neighbors, dead cell stays dead
           return 1;
         }
-      }
+    }
   };
   
   render() {
     const cellSize = 9;
     let gridWidthSize = cellSize * this.state.gridWidth;
+    let gridHeightSize = cellSize * this.state.gridHeight;
     let gridWidthPixels = gridWidthSize + 'px';
+    let gridHeightPixels = gridHeightSize + 'px';
     const gridStyles = {
-      width: gridWidthPixels
+      width: gridWidthPixels,
+      height: gridHeightPixels
     };
+    const generatingMessageStyles = {
+      height: gridHeightPixels,
+      textAlign: 'center',
+      fontSize: '40px',
+      paddingTop: gridHeightSize / 2
+    }
 
     let gridStructure = '';
-    if (this.state.grid !== 'undefined') {
-       gridStructure = this.state.grid.map((cell, i) => {
-        const cellStatus = cell === 0 ? "alive" : "dead";
-        return (<Cell status={cellStatus} index={i} key={i} />)
-      });
-    }
-    return (
-     <div style={gridStyles} className="game-container">
-        {gridStructure}
-        <ControlButtons updateSpecs={this.updateGameGridSize} pause={this.pause}/>
-     </div>
-    );
-  }
-};
-
-class Cell extends Component {
-  constructor(props) {
-    super();
-  }
-  render() {
-    const cellClasses = "cell " + this.props.status + " cell" + this.props.index;
+    
     const aliveStyle = {
       backgroundColor: 'red'
     };
+
     const deadStyle = {
-      backgroundColor: 'white' 
+      backgroundColor: 'beige' 
     };
+
+    const aliveOldStyle = {
+      backgroundColor: 'purple'
+    };
+
+    if (this.state.grid.length === 0) {
+      gridStructure = (<div style={generatingMessageStyles}>Generating new board</div>);
+    } else {
+      if (this.state.grid !== 'undefined') {
+         gridStructure = this.state.grid.map((cell, i) => {
+          const cellStatus = cell === 0 ? "alive" : cell === 1 ? "dead" : "old alive";
+          const cellClasses = "cell " + cellStatus;
+          return (<div key={i} className={cellClasses} style={cellStatus === 'alive' ? aliveStyle : cellStatus === 'old alive' ? aliveOldStyle : deadStyle}></div>)
+        });
+      }
+    }
     return (
-      <div className={cellClasses} style={this.props.status === 'alive' ? aliveStyle : deadStyle}></div>
+     <div style={gridStyles} className="game-container">
+        <div className="game-cells" key="grid-struc" style={gridStyles}>{gridStructure}</div>
+        <ControlButtons updateSpecs={this.updateGameGridSize} pause={this.togglePause} pauseToggle={this.state.pauseToggle}/>
+        <span>Generation: {this.state.generationCount}</span>
+     </div>
     );
   }
 };
